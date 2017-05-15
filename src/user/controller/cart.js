a553a64d3c4023315962cb6b5a9549dcd576d790
CrazyYounghaha <568524975@qq.com>
2017-05-15 09:46:32 +0800

    新增用户订单查询功能

--- src/user/controller/cart.js
+++ src/user/controller/cart.js
@@ -14,8 +14,8 @@ export default class extends Base {
 		yield this.weblogin();
 		if (this.isPost()) {
 			let get = this.post();
-			let product = yield this.model('product').where({product_id: get.productId}).find();
-			let shoppingCart = yield this.model('shoppingcart').where({user_id: this.user.id}).find();
+			let product = yield this.model('product').where({product_id: get.productId}).find();//获取产品详情
+			let shoppingCart = yield this.model('shoppingcart').where({user_id: this.user.id}).find();//获取购物车信息如果有的话
 			if (think.isEmpty(shoppingCart)) {//第一次添加购物车信息
 				let addNewCart = yield this.model('shoppingcart').add({
 					add_time: dateformat('Y-m-d H:i:s', new Date().valueOf()),
@@ -42,7 +42,7 @@ export default class extends Base {
 						shoppingcart_id: shoppingCart.shoppingcart_id
 					}).update({product_count: cartDetail.product_count});
 				} else {
-					yield this.model('shoppingcart').where({user_id: this.user.id}).update({total: shoppingCart.total++});
+					yield this.model('shoppingcart').where({user_id: this.user.id}).update({total: parseInt(shoppingCart.total) + 1});
 					yield this.model('cartdetail').add({
 						price: product.price,
 						product_count: get.number,
@@ -89,7 +89,6 @@ export default class extends Base {
 					total_count: total_count,
 					total_pay: data.total,
 					order_time: dateformat('Y-m-d H:i:s', new Date().valueOf()),
-					pay_type: 1,
 					status: 0,
 					shoppingcart_id: getS_Id.shoppingcart_id,
 					user_id: this.user.id
@@ -126,13 +125,13 @@ export default class extends Base {
 			}).join({
 				table: "product",
 				join: "left",
-				on: ["product_id","product_id"]
+				on: ["product_id", "product_id"]
 			}).where({user_id: this.user.id}).select();
 			console.log(orderDetail);
 			let address = yield this.model('address').where({user_id: this.user.id}).select();
 			console.log(address);
-			this.assign("address",address);
-			this.assign("orderDetail",orderDetail);
+			this.assign("address", address);
+			this.assign("orderDetail", orderDetail);
 			this.assign("style", "pay");
 			return this.display();
 		}
@@ -140,12 +139,71 @@ export default class extends Base {
 
 	* successAction() {
 		yield this.weblogin();
-		this.assign("style", "success");
-		return this.display();
+		if (this.isPost()) {//提交订单处理，减少库存，删除购物车对应项目
+			let address_id = this.post();
+			let toHistory = yield this.model('order').where({user_id: this.user.id}).select();
+			let id = yield this.model('order_history').add({//新增订单历史
+				order_count: toHistory[0].total_count,
+				order_pay: toHistory[0].total_pay,
+				pay_time : dateformat('Y-m-d H:i:s', new Date().valueOf()),
+				pay_type: 1,
+				status: 1,
+				user_id: this.user.id,
+				address_id: address_id.address_id
+			});
+			console.log(id);
+			let getOrderDetails = yield this.model('order').join({
+				table: "orderdetail",
+				join: "left",
+				on: ["order_id", "order_id"]
+			}).where({user_id: this.user.id}).select();
+			// console.log(getOrderDetails.length);//2
+			for (let index in getOrderDetails) {
+				//更新库存
+				yield this.model('product').where({product_id: getOrderDetails[index].product_id}).decrement("inventory", parseInt(getOrderDetails[index].count));
+				yield this.model('product').where({product_id: getOrderDetails[index].product_id}).increment("cumulative_sales", parseInt(getOrderDetails[index].count));
+				//删除orderdetail信息
+				yield this.model('orderdetail').where({
+					order_id: getOrderDetails[index].order_id,
+					product_id: getOrderDetails[index].product_id
+				}).delete();
+				//同时新增一条order_history_detail
+				yield this.model('order_history_detail').add({
+					count: getOrderDetails[index].count,
+					product_id: getOrderDetails[index].product_id,
+					order_history_id: id
+				});
+				//删除cartdetail信息
+				yield this.model('cartdetail').where({
+					shoppingcart_id: getOrderDetails[index].shoppingcart_id,
+					product_id: getOrderDetails[index].product_id
+				}).delete();
+				//购物车数量减
+				yield this.model('shoppingcart').where({user_id: this.user.id}).decrement("total");
+				this.user.cart_total = parseInt(this.user.cart_total) - 1;
+			}
+			//删除订单信息
+			yield this.model('order').where({user_id: this.user.id}).delete();
+			//删除购物车信息
+			if (this.user.cart_total == 0) {
+				yield this.model('shoppingcart').where({user_id: this.user.id}).delete();
+			}
+			return this.success(1);
+		} else {//get显示界面
+			let message = yield this.model('order_history').join({
+				table: "address",
+				join: "left",
+				on: ["address_id","address_id"]
+			}).limit(1).where('ecp_order_history.user_id='+this.user.id).order({pay_time: 'DESC'}).select();
+			console.log(message);
+			this.assign("style", "success");
+			this.assign("payDetail",message);
+			return this.display();
+		}
 	}
 
-	* addressaddAction(){
-		if(this.isPost()){
+	* addressaddAction() {
+		if (this.isPost()) {
 			let post = this.post();
 			console.log(post);
 			let checkExist = yield this.model('address').where({
@@ -158,9 +216,9 @@ export default class extends Base {
 				full_address: post.full_address
 			}).select();
 			// console.log(checkExist);
-			if(!think.isEmpty(checkExist)){
+			if (!think.isEmpty(checkExist)) {
 				return this.success(-1);//已存在一样的地址无需重复添加；
-			}else {//任何信息不同就创建新的
+			} else {//任何信息不同就创建新的
 				yield this.model('address').add({
 					receiver_name: post.receive,
 					phone: post.phone,
--- src/user/controller/person.js
+++ src/user/controller/person.js
@@ -28,12 +28,33 @@ export default class extends Base {
 	}
 	* orderAction(){
 		yield this.weblogin();
+		let orderHistoryDetail = yield this.model('order_history_detail').join({
+			table: "order_history",
+			join: "left",
+			on: ["order_history_id","order_history_id"]
+		}).join({
+			table: "product",
+			join: "left",
+			on: ["product_id","product_id"]
+		}).where('ecp_order_history.user_id='+this.user.id).order({pay_time:"DESC"}).select();
+		console.log(orderHistoryDetail);
+		this.assign("order_history",orderHistoryDetail);
 		this.assign("style","order");
 		return this.display();
 	}
 	* commentAction(){
 		yield this.weblogin();
 		this.assign("style","comment");
+		return this.display();
+	}
+	* orderinfoAction(){
+		yield this.weblogin();
+		this.assign("style","order");
+		return this.display();
+	}
+	* expressAction(){
+		yield this.weblogin();
+		this.assign("style","express");
 		return this.display();
 	}
 	* consultationAction(){
