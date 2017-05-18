5ecf4973b56796a28c2d745b4179c33e193c36d7
suyixin45 <514738894@qq.com>
2017-05-19 00:41:52 +0800

    create user and admin management

--- src/admin/controller/privilege.js
+++ src/admin/controller/privilege.js
@@ -7,17 +7,80 @@ export default class extends Base {
      * index action
      * @return {Promise} []
      */
-    addadminAction(){
+    * addadminAction() {
         //auto render template file index/index.html
-        this.assign("style", "privilege");
-        return this.display();
+        if (this.isPost()) {
+            let addInfo = this.post();
+            //console.log(addInfo);
+            let check = yield this.model('admin').where({name: addInfo.adminName}).select();
+            //console.log(check);
+            if (think.isEmpty(check)) { //如果不存在这个管理员就新增
+                addInfo.adminpwd = encryptPassword(addInfo.adminpwd);
+                yield this.model('admin').add({
+                    name: addInfo.adminName,
+                    password: addInfo.adminpwd,
+                    email: addInfo.adminemail,
+                    phone: addInfo.adminphone
+                });
+
+                return this.success(1);
+
+            }
+            return this.success(-1); //已存在同名管理员无法添加
+        } else {
+            let adminInfo = yield this.model('admin').select();
+            this.assign('adminInfo', adminInfo);
+            this.assign("style", "privilege");
+            return this.display();
+        }
+
     }
 
-    listadminAction(){
-        this.assign("style", "privilege");
-        return this.display();
+    * changeadminAction() {
+        if (this.isPost()) {
+            let changeInfo = this.post();
+            //console.log(changeInfo);
+            let check = yield this.model('admin').where({admin_id:changeInfo.admin_id}).select();
+            //console.log(check);
+            if(!think.isEmpty(check)) {  //如果存在该管理员就修改
+                yield this.model('admin').where({admin_id:changeInfo.admin_id}).update({
+                    name: changeInfo.changeName,
+                    password: changeInfo.changePwd,
+                    email: changeInfo.changeEmail,
+                    phone: changeInfo.changePhone
+                });
+
+                return this.success(1);
+
+            }
+            return this.success(-1);   //不存在该管理员无法修改
+        }else {
+            let  adminInfo = yield this.model('admin').select();
+            this.assign('adminInfo',adminInfo);
+            this.assign('style',"privilege");
+            return this.display();
+        }
+
+
+
+
+
+
+        }
+        * deleteadminAction() {
+        if(this.isPost()) {
+            let delete_admin_id = this.post('admin_id');
+            // console.log(delete_admin_id);
+            yield this.model('admin').where({admin_id:delete_admin_id}).delete();
+        }
+
+        }
+
+
     }
-}/**
+
+
+/**
  * Created by Administrator on 2017/4/18.
  */
 
--- src/admin/controller/vip.js
+++ src/admin/controller/vip.js
@@ -10,9 +10,38 @@ export default class extends Base {
 	 * index action
 	 * @return {Promise} []
 	 */
-	indexAction(){
+	* indexAction(){
 		//auto render template file index/index.html
-		this.assign("style","vip");
-		return this.display();
+        let userInfo = yield this.model('user').select();
+        this.assign('userInfo',userInfo);
+        this.assign("style","vip");
+        return this.display();
 	}
+
+
+	* addvipAction(){
+		if(this.isPost()) {
+            let vipInfo = this.post();
+            // console.log(vipInfo);
+            let check = yield this.model('user').where({name: vipInfo.vipName}).select();
+             //console.log(check);
+            if (think.isEmpty(check)) {  //如果不存在这个用户就新增
+                vipInfo.vipPassword = encryptPassword(vipInfo.vipPassword);
+                yield this.model('user').add({
+                    name: vipInfo.vipName,
+                    password: vipInfo.vipPassword,
+                    email: vipInfo.vipEmail,
+                    phone: vipInfo.vipPhone,
+                    is_online: vipInfo.vipLogin,
+                    is_vip: vipInfo.vipStatus
+                });
+                return this.success(1);
+            }
+            return this.success(-1);  //已存在同名会员无法添加
+        }else{
+
+
+		}
+	}
+
 }
