fbed1b99385f21b09a03eb5cdb169dbc2cf83993
CrazyYounghaha <568524975@qq.com>
2017-05-23 13:02:12 +0800

    商品详情界面的评论显示

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -7,11 +7,11 @@ export default {
   type: 'mysql',
   adapter: {
     mysql: {
-      host: '127.0.0.1',//118.89.174.188
+      host: '118.89.174.188',//118.89.174.188
       port: '',
       database: 'ecp',
       user: 'root',
-      password: '',//A608_2017
+      password: 'A608_2017',//A608_2017
       prefix: 'ecp_',
       encoding: 'utf8'
     },
--- src/user/controller/introduction.js
+++ src/user/controller/introduction.js
@@ -18,13 +18,23 @@ export default class extends Base {
 			let pictures = yield this.model('picture').where({product_id: productId.productId,type: 1}).select();
 			// console.log(pictures);
 			let productDetails = yield this.model('product').where({product_id: productId.productId}).find();
-			let comment_number = yield this.model('feedback').where({product_id: productId.productId}).count()
-			// console.log(comment_number);
+			let comment_number = yield this.model('feedback').where({product_id: productId.productId}).count();
+			let comment_detail = yield this.model('feedback').join({
+					table: "product",
+					join: "left",
+					on: ["product_id","product_id"]
+			}).join({
+					table: "user",
+					join: "left",
+					on: ["user_id","user_id"]
+			}).where('ecp_feedback.product_id='+productId.productId).field('ecp_feedback.*,ecp_product.name as product_name,ecp_product.detail as product_detail,ecp_user.name as user_name').select();
+			console.log(comment_detail);
 			// console.log(productDetails);
 			this.assign("style","introduction");
 			this.assign("product_details",productDetails);
 			this.assign("commentNumber",comment_number);
 			this.assign("pictures", pictures);
+			this.assign("comment", comment_detail);
 			return this.display();
 		}
 	}
